381b2da24cf1cdc57228628b22fd0c4db551fd1e
Vincent Renais <vincentrenais@gmail.com>
2015-04-08 15:43:44 -0400

    Second commit

--- bml_day3_a1/main.m
+++ bml_day3_a1/main.m
@@ -8,16 +8,53 @@
 
 #import <Foundation/Foundation.h>
 
-int getRandomInteger (int minimum, int maximum) {
+// BASIC SYNTAX
+
+/*
+ int getRandomInteger (int minimum, int maximum) {
     return arc4random_uniform((maximum - minimum) + 1) + minimum;
 }
+*/
+
+/*
+NSString *getRandomMake (NSArray *makes) {
+    int maximum = (int)[makes count];
+    int randomIndex = arc4random_uniform(maximum);
+    return makes[randomIndex];
+}
+ */
+
+
+// Declaration
+
+NSString *getRandomMake(NSArray *);
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
+        // BASIC SYNTAX
+        
+        /*
         int randomNumber = getRandomInteger(-10, 10);
         NSLog(@"Selected a random number between -10 and 10: %d", randomNumber);
+        */
+        
+        
+        NSArray *makes = @[@"Honda", @"Ford", @"Nissan", @"Porche"];
+        NSLog(@"Selected a %@", getRandomMake(makes));
+        
         
     }
     return 0;
 }
+
+
+// Implementation
+
+
+ NSString *getRandomMake (NSArray *makes) {
+ int maximum = (int)[makes count];
+ int randomIndex = arc4random_uniform(maximum);
+ return makes[randomIndex];
+ }
